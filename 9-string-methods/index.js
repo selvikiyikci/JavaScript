3ef3bd850121d40079a7b7e09e2c896607330ec1
selvikiyikci <selvikiyikci@gmail.com>
2024-07-05 00:53:57 +0300

    Rest of the code

---
+++ 9-string-methods/index.js
@@ -0,0 +1,10 @@
+let youtube = "Bilgisayar Genetiği";
+let result;
+
+result = youtube.toLowerCase();
+result = youtube.toUpperCase();
+result  = youtube.length;
+result = youtube.trim();
+result = youtube.slice(0,6);
+result = youtube.split(" ");
+console.log(result);

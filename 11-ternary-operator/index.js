4ab32e831c84dee2b734e8040f78cc50c5980143
selvikiyikci <selvikiyikci@gmail.com>
2024-07-05 17:20:28 +0300

    Number methods
    Application fo Number methods in js

--- 11-ternary-operator/index.js
+++ 11-ternary-operator/index.js
@@ -4,7 +4,7 @@ const password =  "123456";
 
 
 password.length > 6 ? console.log("çalıştı.") : console.log("çalışmadı.");
-
+username.length > 11 ? console.log("username doğru") : console.log("doğru değil");
 /*  if(password.length > 6) {
     console.log("çalıştı");
 }

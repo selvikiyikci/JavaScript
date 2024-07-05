723e9df8699c88d1de10b6d0c41ad9e44025d04e
selvikiyikci <selvikiyikci@gmail.com>
2024-07-05 18:02:12 +0300

    Arrays
    Fundamentals of arrays

---
+++ 14-arrays/index.js
@@ -0,0 +1,6 @@
+const urunler = ["samsung", "apple", "huawei"];
+const fiyatlar = [10, 20, 30];
+const urun1 = ["samsung", 10];
+const urun2 = ["apple", 20];
+const yeniUrun = ["samsung", 150, ["apple", 50]];
+console.log(`${urunler[2]} ${fiyatlar[2]}`);

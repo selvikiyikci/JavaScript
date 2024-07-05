fbaa55640e5d936d2e36074ecbba50c3f6eddcbf
selvikiyikci <selvikiyikci@gmail.com>
2024-07-05 16:58:10 +0300

    Ternary Operator
    Quick example

---
+++ 11-ternary-operator/index.js
@@ -0,0 +1,14 @@
+const username = "eminbasbayan";
+const password =  "123456";
+
+
+
+password.length > 6 ? console.log("çalıştı.") : console.log("çalışmadı.");
+
+/*  if(password.length > 6) {
+    console.log("çalıştı");
+}
+else {
+    console.log("6'dan büyük olmadığı için çalışmadı.");
+}
+ */
